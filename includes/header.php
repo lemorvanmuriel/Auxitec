0d2441789ab2f487c3fa0ba95f3aea3c4f8d3c98
Lemorvanmuriel <muriel.lemorvan@laposte.net>
2018-03-06 11:20:11 +0100

    création header.php

---
+++ includes/header.php
@@ -0,0 +1,7 @@
+<header>
+<nav>
+    <li><a href="index.php?page=accueil">Accueil</a></li>
+    <li><a href="index.php?page=inscription">Inscription</a></li>
+    <li><a href="index.php?page=login">Login</a></li>
+</nav>
+</header>
