301d0e01bc11720f5a092a1b0e2bb62bf09f0eea
Lemorvanmuriel <muriel.lemorvan@laposte.net>
2018-03-06 11:46:01 +0100

    index ajout de header et footer

--- index.php
+++ index.php
@@ -5,4 +5,9 @@
  * Date: 06/03/2018
  * Time: 10:22
  */
-phpinfo();
+//phpinfo();
+include "./includes/header.php";
+
+echo '<h1>Ceci est le corps de mon index.php</h1>';
+
+include "./includes/footer.php";
