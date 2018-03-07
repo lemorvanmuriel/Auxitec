feedce08e0872b97f2a91715b220e83dfc9a77ba
Lemorvanmuriel <muriel.lemorvan@laposte.net>
2018-03-07 09:49:45 +0100

    frminscription.php

---
+++ includes/frminscription.php
@@ -0,0 +1,27 @@
+<form method="post" action="#">
+<div>
+    <label for="nom">Nom: </label>
+    <input type="text" name="nom" />
+</div>
+<div>
+    <label for="prenom">Prénom: </label>
+    <input type="text" name="prenom" />
+</div>
+<div>
+    <label for="mail">Mail: </label>
+    <input type="text" name="mail" />
+</div>
+<div>
+    <label for="mdp">Mot de passe: </label>
+    <input type="pasword" name="mdp" />
+</div>
+
+<div>
+    <input type="reset" value="Effacer" />
+    <input type="submit" value="Envoyer" />
+    <input type="hidden" name="frmInscription"/>
+    <!--   <input type="hidden" value="42" name="frmInscription"/>    champ caché permet de passer des valeurs -->
+
+</div>
+
+</form>
